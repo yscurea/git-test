d3efbd652c6c9731ee55a9a3cbd5af9f2194307c
yuusuine <yuuki.990510@gmail.com>
2020-02-18 16:06:31 +0900

    add function

--- layers.py
+++ layers.py
@@ -66,21 +66,39 @@ class Affine:
 
         return dx
 
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arrange(batch_size), t] + 1e-7)) / batch_size
+
+
 class SoftmaxWithLoss:
     def __init__(self):
-        self.loss=None
-        self.y=None
-        self.t=None
+        self.loss = None
+        self.y = None
+        self.t = None
 
-    def forward(self,x,t):
-        self.t=t
-        self.y=softmax(x)
-        self.loss=cross_entropy_error(self.y,self.t)
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
 
         return self.loss
 
-    def backward(self,dout=1):
-        batch_size=self.t.shape[0]
-        dx=(self.y-self.t)/batch.size
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch.size
 
         return dx
